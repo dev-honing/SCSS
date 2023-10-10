0be40aef4a87ae25c276d2da38da98093851bf8a
Tech1997 <dev.honing@gmail.com>
2023-10-10 13:59:24 +0900

    문자열로 HTML 요소를 ElementHTML로 변수 선언

--- app.js
+++ app.js
@@ -6,58 +6,31 @@ const fs = require('fs');
 // 포켓몬 모듈에서 목록 가져오기
 const pokemonKoreanArr = pokemon.all('ko');
 
-// HTML 요소를 ElementHTML로 변수 선언
-const ElementHTML_front = `
+// 문자열로 HTML 요소를 변수 선언
+const ElementHTML = 
+`
 <html>
   <head>
   </head>
   <body>
-    <li>`
-    
-const ElementHTML_back =    
-    `
-    </li>
+  <h1>
+  </h1>
   </body>
-  </html>`;
-  
-// HTML 요소와 포켓몬 하나씩을 합치기
-function pokemonHTML(){
-  for (i=0; i < pokemonKoreanArr.length; i++){
-    pokemonWithHTML = ElementHTML_front + pokemonKoreanArr[i] + ElementHTML_back;
-  }
-pokemonHTML();
-}
-// toString() 메서드로 함수를 문자열화할 수 있는지 확인 - 실패
-
-const pokemonString = toString(pokemonHTML());
-console.log(typeof(pokemonString));
+</html>
+`
+console.log(ElementHTML); // 확인
 
 
 
 
-fs.writeFile("pokemon.html", pokemonWithHTML , (err) => {
-  if (err){
-    console.log(err);
-  }
-  else {
-    console.log("파일 생성완료\n");
-    console.log("컨텐츠를 불러왔습니다.");
-    console.log(fs.readFileSync("pokemon.html", "utf-8"));
-  }
-});
 
-// 오류 코드
-// PS C:\Users\Ho\Desktop\SCSS> node app.js
-//                     ^
-
-// RangeError: Maximum call stack size exceeded
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:24:21)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
-//     at pokemonHTML (C:\Users\Ho\Desktop\SCSS\app.js:28:1)
+// fs.writeFile("pokemon.html", pokemonWithHTML , (err) => {
+//   if (err){
+//     console.log(err);
+//   }
+//   else {
+//     console.log("파일 생성완료\n");
+//     console.log("컨텐츠를 불러왔습니다.");
+//     console.log(fs.readFileSync("pokemon.html", "utf-8"));
+//   }
+// });
