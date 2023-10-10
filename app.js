a832070dd27ab4842488c99c6afe8d53d0618f72
Tech1997 <dev.honing@gmail.com>
2023-10-10 12:02:13 +0900

    Q: 포켓몬 목록을 HTML의 <li>태그로 넣을 수 있나요?

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const pokemon = require('pokemon');
-// 포켓몬 전체 리스트를 확인할 변수를 선언
-let pokemonList = pokemon.all();
-// console.log(pokemonList); 
 
-// 포켓몬 이름을 한글화 하는 변수 선언
-let pokemonToKorean = pokemon.all('ko');
-console.log(pokemonToKorean); // node app.js 실행하고 한글화되는지 확인
