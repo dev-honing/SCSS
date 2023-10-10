21c0483e7c658889ea62d9abf4de96f090cc607f
Tech1997 <dev.honing@gmail.com>
2023-10-10 12:32:34 +0900

    화살표 함수로 조건문 로직 작성

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const fs = require('fs');
 // 포켓몬 모듈에서 목록 가져오기
 const pokemonKorean = pokemon.all('ko');
 
-fs.writeFile("pokemon.html", data, );
 // HTML 요소와 한글화된 목록을 pokemonData로 변수 선언
 const pokemonData = `<html><head></head><body><li>`+ pokemonKorean +`</li></body></html>`
+fs.writeFile("pokemon.html", pokemonData , (err) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("파일 생성완료\n");
+    console.log("컨텐츠를 불러왔습니다.");
+    console.log(fs.readFileSync("pokemon.html", "utf-8"));
+  }
+});
