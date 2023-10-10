0e6f6b0dcd7b153a9d8f7961159c4c0386d51524
Tech1997 <dev.honing@gmail.com>
2023-10-10 12:06:32 +0900

    포켓몬 모듈에서 한글로 목록 가져오고 콘솔 확인

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 const pokemon = require('pokemon');
 // 파일 시스템 모듈 import
 const fs = require('fs');
+
+// 포켓몬 모듈에서 목록 가져오기
+const pokemonKorean = pokemon.all('ko');
+console.log(pokemonKorean); // 확인
