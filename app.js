d2e03819865f14bb101f708005713d0f6a4dde0e
Tech1997 <dev.honing@gmail.com>
2023-10-10 12:38:30 +0900

    가져온 목록의 변수명을 pokemonKoreanArr로 정정

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const pokemon = require('pokemon');
 const fs = require('fs');
 
 // 포켓몬 모듈에서 목록 가져오기
-const pokemonKorean = pokemon.all('ko');
+const pokemonKoreanArr = pokemon.all('ko');
 
 // HTML 요소와 한글화된 목록을 pokemonData로 변수 선언
-const pokemonData = `<html><head></head><body><li>`+ pokemonKorean +`</li></body></html>`
+const pokemonData = `<html><head></head><body><li>`+ pokemonKoreanArr +`</li></body></html>`
 fs.writeFile("pokemon.html", pokemonData , (err) => {
   if (err)
     console.log(err);
