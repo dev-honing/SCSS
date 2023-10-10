989b11b08395a83c55e4a2a64b52ffdd8320cb19
Tech1997 <dev.honing@gmail.com>
2023-10-10 11:58:58 +0900

    포켓몬 이름을 한글화하기 위한 변수 선언

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const pokemon = require('pokemon');
 // 포켓몬 전체 리스트를 확인할 변수를 선언
 let pokemonList = pokemon.all();
 
-console.log(pokemonList); // node app.js로 실행해서 콘솔 확인
+console.log(pokemonList); 
+
+// 포켓몬 이름을 한글화 하는 변수 선언
+let pokemonToKorean = pokemon.all('ko');
+
